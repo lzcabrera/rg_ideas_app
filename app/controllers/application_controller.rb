1e41da53729744b12681687d6bfd27d20913cc9f
Laura Cabrera <lzcabrera@gmail.com>
2014-02-09 20:21:30 -0800

    added logic to force use to redirect to login if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # force user to redirect to login page if user was not logged in
+  before_action :authenticate_user!
 end
